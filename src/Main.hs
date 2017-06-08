446ae334c5d8d78c028599e96547a901565b6ab3
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-07 18:07:58 -0700

    Work on ninja2nix

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Main where
 
--- import           Control.Lens        as Lens
+import           Control.Lens               as Lens hiding
+                 ((.=), (.>), (<.), (<|), (|>))
 
 import qualified Data.Makefile              as Makefile
 import qualified Data.Makefile.Parse        as Makefile
@@ -13,6 +15,7 @@ import qualified Data.Makefile.Parse        as Makefile
 
 import           Control.Arrow
 import           Data.Either
+import           Data.List                  (sort)
 import           Data.Maybe
 import           Data.Monoid
 
@@ -50,8 +53,10 @@ import qualified Ninja.Type                 as Ninja
 import           Ninja.Eval
                  (Command (..), RuleName (..), Target (..))
 
-import           Data.Aeson
-import           Data.Aeson.Encode.Pretty
+import           Data.Aeson                 as Aeson
+import           Data.Aeson.Encode.Pretty   as Aeson
+
+import           Flow
 
 import           Hash
 
@@ -80,34 +85,26 @@ commandFromBS = MkCommand . T.decodeUtf8
 -- | A simplified build graph.
 data SNinja
   = MkSNinja
-    { _snBuilds   :: HashMap (HashSet Target) SBuild
-    , _snDefaults :: HashSet Target
+    { _snBuilds   :: !(HashMap Target SBuild)
+    , _snDefaults :: !(HashSet Target)
     }
   deriving (Eq, Show)
 
--- | Lint the given build graph.
-checkSNinja :: SNinja -> Bool
-checkSNinja (MkSNinja builds defaults) = and [ noOverlappingOutputs
-                                             ]
-  where
-    noOverlappingOutputs = let outs = HM.keys builds
-                           in HS.size (mconcat outs) == sum (map HS.size outs)
-
 -- | Look up the unique build rule that outputs the given target.
-lookupBuild :: SNinja -> Target -> Maybe (HashSet Target, SBuild)
-lookupBuild (MkSNinja builds _) target
-  = case HM.toList (HM.filterWithKey (\k _ -> HS.member target k) builds)
-    of [result] -> Just result
-       []       -> Nothing
-       _        -> error "invalid SNinja: overlapping keys"
+lookupBuild :: SNinja -> Target -> Maybe SBuild
+lookupBuild (MkSNinja builds _) target = HM.lookup target builds
+
+-- | Compute the set of all targets that are an output of a rule.
+allOutputs :: SNinja -> HashSet Target
+allOutputs (MkSNinja builds _) = HS.fromList (HM.keys builds)
+
+-- | Compute the set of all targets that are a dependency to a rule.
+allInputs :: SNinja -> HashSet Target
+allInputs (MkSNinja builds _) = mconcat $ map (_sbDeps . snd) $ HM.toList builds
 
 -- | Compute the set of all targets referenced in the build graph.
 allTargets :: SNinja -> HashSet Target
-allTargets (MkSNinja builds defaults) = outputs <> defaults <> dependencies
-  where
-    outputs, dependencies :: HashSet Target
-    outputs      = mconcat $ HM.keys builds
-    dependencies = mconcat $ map (_sbDeps . snd) $ HM.toList builds
+allTargets (sn@(MkSNinja _ defs)) = defs <> allOutputs sn <> allInputs sn
 
 -- | Compute the set of all commands that can be run during a build.
 allCommands :: SNinja -> HashSet Command
@@ -120,14 +117,14 @@ leafTargets :: SNinja -> HashSet Target
 leafTargets (sn@(MkSNinja builds _)) = HS.difference (allTargets sn) outputs
   where
     outputs :: HashSet Target
-    outputs = mconcat $ HM.keys builds
+    outputs = HS.fromList $ HM.keys builds
 
 -- | Compute the set of targets that the given target depends on.
 targetReferences :: SNinja -> Target -> HashSet Target
 targetReferences (sn@(MkSNinja builds _)) target
-  = case lookupBuild sn target
-    of Just (_, MkSBuild _ deps) -> deps
-       Nothing                   -> HS.empty
+  = case lookupBuild sn target of
+      Just (MkSBuild _ deps) -> deps
+      Nothing                -> HS.empty
 
 -- | Compute the set of targets that depend on the given target.
 targetReferrers :: SNinja -> Target -> HashSet Target
@@ -145,9 +142,10 @@ data SBuild
 compileNinja :: Ninja.Ninja -> SNinja
 compileNinja (Ninja.MkNinja {..}) = MkSNinja simpleBuilds simpleDefaults
   where
-    simpleBuilds :: HashMap (HashSet Target) SBuild
-    simpleBuilds = HM.map simplifyBuild combined
-                   <> HM.fromList (map simplifyPhony phonys)
+    simpleBuilds :: HashMap Target SBuild
+    simpleBuilds = [ HM.map simplifyBuild combined
+                   , HM.fromList (map simplifyPhony phonys)
+                   ] |> mconcat |> linearizeGraph
 
     simpleDefaults :: HashSet Target
     simpleDefaults = HS.map targetFromBS $ HS.fromList defaults
@@ -182,40 +180,50 @@ compileNinja (Ninja.MkNinja {..}) = MkSNinja simpleBuilds simpleDefaults
                $ map (first (HS.fromList . map targetFromBS))
                $ multiples <> map (first (\x -> [x])) singles
 
-newtype Path
-  = MkPath Text
-  deriving ()
+    linearizeGraph :: HashMap (HashSet Target) SBuild -> HashMap Target SBuild
+    linearizeGraph = HM.toList
+                     .> map (first HS.toList)
+                     .> concatMap linearizeNode
+                     .> HM.fromList
 
-data DerivationType
-  = NormalDerivation
-  | FixedOutputDerivation !(Digest SHA256)
-  deriving ()
+    linearizeNode :: ([Target], SBuild) -> [(Target, SBuild)]
+    linearizeNode (outs, build)
+      = case uncons (sort outs) of
+          Nothing           -> []
+          Just (root, rest) -> (root, build) : map (`createEdge` root) rest
 
-data Derivation
-  = MkDerivation
-    { _drvName    :: !Text
-    , _drvOutputs :: !(HashSet Text)
-    , _drvBuilder :: !Path
-    , _drvArgs    :: ![Text]
-    , _drvInputs  :: !(HashSet Derivation)
-    , _drvEnv     :: !(HashMap Text Text)
-    , _drvType    :: !DerivationType
-    }
-  deriving ()
+    createEdge :: Target -> Target -> (Target, SBuild)
+    createEdge x y = (x, MkSBuild Nothing (HS.singleton y))
+
+sninjaToJSON :: SNinja -> Value
+sninjaToJSON (sn@(MkSNinja {..})) = [ "graph" .= builds, "defaults" .= defaults
+                                    ] |> object
+  where
+    builds, defaults :: Value
+    builds   = toJSON (HM.mapWithKey pairJ (HM.map Just _snBuilds <> leafs))
+    defaults = toJSON _snDefaults
+
+    leafs :: HashMap Target (Maybe SBuild)
+    leafs = leafTargets sn
+            |> HS.toList
+            |> map (\t -> (t, Nothing))
+            |> HM.fromList
+
+    pairJ :: Target -> Maybe SBuild -> Value
+    pairJ _      (Just (MkSBuild (Just c) deps)) = buildJ c deps
+    pairJ _      (Just (MkSBuild Nothing  deps)) = phonyJ deps
+    pairJ target Nothing                         = leafJ target
+
+    buildJ :: Command -> HashSet Target -> Value
+    buildJ c deps = tyObject "build" ["dependencies" .= deps, "command" .= c]
+
+    phonyJ :: HashSet Target -> Value
+    phonyJ deps = tyObject "phony" ["dependencies" .= deps]
+
+    leafJ :: Target -> Value
+    leafJ target = tyObject "leaf" ["path" .= target]
+
+    tyObject t rest = object (("type" .= (t :: Text)) : rest)
 
 main :: IO ()
 main = pure ()
-
-instance ToJSON SNinja where
-  toJSON (MkSNinja {..}) = object ["builds" .= builds, "defaults" .= defaults]
-    where
-      builds, defaults :: Value
-      builds   = toJSON $ map buildPair $ HM.toList _snBuilds
-      defaults = toJSON _snDefaults
-
-      buildPair :: (HashSet Target, SBuild) -> Value
-      buildPair (outs, build) = object ["outputs" .= outs, "build" .= build]
-
-instance ToJSON SBuild where
-  toJSON (MkSBuild Nothing  deps) = object ["phony" .= deps]
-  toJSON (MkSBuild (Just c) deps) = object ["cmd" .= c, "deps" .= deps]
