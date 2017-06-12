0d036e828af2e7da05d5d165abb98afbe5b8fed7
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-12 12:46:26 -0700

    Refactoring in Language.Ninja.Parse

--- library/Language/Ninja/Parse.hs
+++ library/Language/Ninja/Parse.hs
@@ -29,8 +29,10 @@
 -- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 -- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TupleSections   #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Language.Ninja.Parse
   ( parse, parseWithEnv
@@ -39,11 +41,14 @@ module Language.Ninja.Parse
 import           Control.Applicative
 import           Control.Monad
 import qualified Data.ByteString.Char8 as BS
+import           Data.Monoid
 import           Prelude
 
 import           Language.Ninja.Env
 import           Language.Ninja.Lexer
 import           Language.Ninja.Types
+
+import           Flow
 
 parse :: FilePath -> IO Ninja
 parse file = newEnv >>= parseWithEnv file
@@ -53,64 +58,65 @@ parseWithEnv file env = parseFile file env newNinja
 
 parseFile :: FilePath -> Env Str Str -> Ninja -> IO Ninja
 parseFile file env ninja = do
-    lexes <- lexerFile $ if file == "-" then Nothing else Just file
-    foldM (applyStmt env) ninja $ withBinds lexes
+  lexes <- lexerFile $ if file == "-" then Nothing else Just file
+  foldM (applyStmt env) ninja $ withBinds lexes
 
-withBinds :: [Lexeme] -> [(Lexeme, [(Str,Expr)])]
+withBinds :: [Lexeme] -> [(Lexeme, [(Str, Expr)])]
 withBinds [] = []
-withBinds (x:xs) = (x,a) : withBinds b
-    where
-        (a,b) = f xs
-        f (LexBind a b : rest) = let (as,bs) = f rest in ((a,b):as, bs)
-        f xs                   = ([], xs)
+withBinds (x:xs) = (x, a) : withBinds b
+  where
+    (a, b) = f xs
+    f ((LexBind a b) : rest) = let (as, bs) = f rest in (((a, b):as), bs)
+    f xs                     = ([], xs)
 
-applyStmt :: Env Str Str -> Ninja -> (Lexeme, [(Str,Expr)]) -> IO Ninja
+applyStmt :: Env Str Str -> Ninja -> (Lexeme, [(Str, Expr)]) -> IO Ninja
 applyStmt env (ninja@(MkNinja {..})) (key, binds) = case key of
-  LexBuild outputs rule deps -> do
+  (LexBuild outputs rule deps) -> do
     outputs <- mapM (askExpr env) outputs
     deps <- mapM (askExpr env) deps
-    binds <- mapM (\(a,b) -> (a,) <$> askExpr env b) binds
-    let (normal,implicit,orderOnly) = splitDeps deps
+    binds <- mapM (\(a, b) -> (a,) <$> askExpr env b) binds
+    let (normal, implicit, orderOnly) = splitDeps deps
     let build = MkBuild rule env normal implicit orderOnly binds
-    pure
-      $ if      rule == BS.pack "phony" then ninja { phonys = [(x, normal ++ implicit ++ orderOnly) | x <- outputs] ++ phonys }
-        else if length outputs == 1     then ninja { singles = (head outputs, build) : singles }
-        else                                 ninja { multiples = (outputs, build) : multiples }
-  LexRule name -> do
+    pure $ if      rule == "phony"     then ninja { phonys = [(x, normal <> implicit <> orderOnly) | x <- outputs] <> phonys }
+           else if length outputs == 1 then ninja { singles = (head outputs, build) : singles }
+           else                             ninja { multiples = (outputs, build) : multiples }
+  (LexRule name) -> do
     pure ninja { rules = (name, MkRule binds) : rules }
-  LexDefault xs -> do
+  (LexDefault xs) -> do
     xs <- mapM (askExpr env) xs
     pure (ninja { defaults = xs ++ defaults })
-  LexPool name -> do
+  (LexPool name) -> do
     depth <- getDepth env binds
     pure (ninja { pools = (name, depth) : pools })
-  LexInclude expr -> do
+  (LexInclude expr) -> do
     file <- askExpr env expr
     parseFile (BS.unpack file) env ninja
-  LexSubninja expr -> do
+  (LexSubninja expr) -> do
     file <- askExpr env expr
     e <- scopeEnv env
     parseFile (BS.unpack file) e ninja
-  LexDefine a b -> do
+  (LexDefine a b) -> do
     addBind env a b
     pure ninja
-  LexBind a _ -> do
-    error $ "Unexpected binding defining " ++ BS.unpack a
+  (LexBind a _) -> [ "Unexpected binding defining ", a
+                   ] |> mconcat |> BS.unpack |> error
 
 splitDeps :: [Str] -> ([Str], [Str], [Str])
-splitDeps [] = ([], [], [])
-splitDeps (x:xs)
-  | x == BS.pack "|"  = ([],  a ++ b,      c)
-  | x == BS.pack "||" = ([],       b, a ++ c)
-  | otherwise         = (x:a,      b,      c)
+splitDeps []                   = ([],      [],     [])
+splitDeps (x:xs) | (x == "|")  = ([],  a <> b,      c)
+                 | (x == "||") = ([],       b, a <> c)
+                 | otherwise   = (x:a,      b,      c)
   where
-    (a,b,c) = splitDeps xs
+    (a, b, c) = splitDeps xs
 
 getDepth :: Env Str Str -> [(Str, Expr)] -> IO Int
-getDepth env xs = case lookup (BS.pack "depth") xs of
-  Nothing -> pure 1
-  Just x -> do
-    x <- askExpr env x
-    case BS.readInt x of
-      Just (i, n) | BS.null n -> pure i
-      _ -> error $ "Could not parse depth field in pool, got: " ++ BS.unpack x
+getDepth env xs = do
+  let poolDepthError x = [ "Could not parse depth field in pool, got: ", x
+                         ] |> mconcat |> BS.unpack |> error
+  case lookup "depth" xs of
+    Nothing -> pure 1
+    Just x -> do
+      x <- askExpr env x
+      case BS.readInt x of
+        (Just (i, n)) | BS.null n -> pure i
+        _                         -> poolDepthError x
