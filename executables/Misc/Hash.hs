51a0320a7c91663248d6640f693f5ba9a78e77de
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-14 13:26:30 -0700

    Add preamble to executables/{NinjaToNix.hs,Misc/Hash.hs}

--- executables/Misc/Hash.hs
+++ executables/Misc/Hash.hs
@@ -1,5 +1,37 @@
-{-# LANGUAGE PackageImports #-}
+-- -*- coding: utf-8; mode: haskell; -*-
 
+-- File: executables/Misc/Hash.hs
+--
+-- License:
+--     Copyright 2017 Awake Networks
+--
+--     Licensed under the Apache License, Version 2.0 (the "License");
+--     you may not use this file except in compliance with the License.
+--     You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--     Unless required by applicable law or agreed to in writing, software
+--     distributed under the License is distributed on an "AS IS" BASIS,
+--     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--     See the License for the specific language governing permissions and
+--     limitations under the License.
+
+{-# OPTIONS_GHC #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PackageImports    #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+-- |
+--   Module      : Misc.Hash
+--   Copyright   : Copyright 2017 Awake Networks
+--   License     : Apache-2.0
+--   Maintainer  : opensource@awakenetworks.com
+--   Stability   : experimental
+--
+--   FIXME: doc
 module Misc.Hash
   ( sha256
   , module Exported
@@ -14,5 +46,6 @@ import qualified Data.Text       as T
 digestSHA256 :: BS.ByteString -> Digest SHA256
 digestSHA256 = hash
 
+-- | FIXME: doc
 sha256 :: BS.ByteString -> T.Text
 sha256 = T.pack . show . digestSHA256
