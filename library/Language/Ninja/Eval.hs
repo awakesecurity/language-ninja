f2a2aec29265d3aa7ca894cbd5069246aa1b96b3
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-14 13:12:20 -0700

    Split Meta off into Language.Ninja.Eval.Meta

--- library/Language/Ninja/Eval.hs
+++ library/Language/Ninja/Eval.hs
@@ -43,6 +43,7 @@ module Language.Ninja.Eval
 import           Control.Arrow
 
 import           Language.Ninja.Eval.Build
+import           Language.Ninja.Eval.Meta
 import           Language.Ninja.Eval.Pool
 import           Language.Ninja.Eval.Rule
 import           Language.Ninja.Eval.Target
@@ -158,72 +159,5 @@ instance FromJSON Ninja where
                   _ninjaDefaults <- (o .: "defaults") >>= pure
                   _ninjaPools    <- (o .: "pools")    >>= pure
                   pure (MkNinja {..}))
-
---------------------------------------------------------------------------------
-
--- | Ninja top-level metadata, as documented
---   <https://ninja-build.org/manual.html#ref_toplevel here>.
-data Meta
-  = MkMeta
-    { _metaReqVersion :: !(Maybe V.SemVer)
-    , _metaBuildDir   :: !(Maybe Path)
-    }
-  deriving (Eq, Ord, Show, Generic)
-
--- | Construct a default 'Meta' value.
-makeMeta :: Meta
-makeMeta = MkMeta
-           { _metaReqVersion = Nothing
-           , _metaBuildDir   = Nothing
-           }
-
--- | Corresponds to the @ninja_required_version@ top-level variable.
-metaReqVersion :: Lens' Meta (Maybe V.SemVer)
-metaReqVersion = lens _metaReqVersion
-                 $ \(MkMeta {..}) x -> MkMeta { _metaReqVersion = x, .. }
-
--- | Corresponds to the @builddir@ top-level variable.
-metaBuildDir :: Lens' Meta (Maybe Path)
-metaBuildDir = lens _metaBuildDir
-               $ \(MkMeta {..}) x -> MkMeta { _metaBuildDir = x, .. }
-
--- | Default 'Hashable' instance via 'Generic'.
-instance Hashable Meta
-
--- | Converts to @{required-version: …, build-directory: …}@.
-instance ToJSON Meta where
-  toJSON (MkMeta {..})
-    = [ "req-version" .= fmap semverJ _metaReqVersion
-      , "build-dir"   .= _metaBuildDir
-      ] |> object
-    where
-      semverJ :: V.SemVer -> Value
-      semverJ = V.prettySemVer .> toJSON
-
--- | Inverse of the 'ToJSON' instance.
-instance FromJSON Meta where
-  parseJSON = (withObject "Meta" $ \o -> do
-                  _metaReqVersion <- (o .: "req-version") >>= maybeSemverP
-                  _metaBuildDir   <- (o .: "build-dir")   >>= pure
-                  pure (MkMeta {..}))
-    where
-      maybeSemverP :: Maybe Value -> Aeson.Parser (Maybe V.SemVer)
-      maybeSemverP = fmap semverP .> sequenceA
-
-      semverP :: Value -> Aeson.Parser V.SemVer
-      semverP = withText "SemVer" (megaparsecToAeson V.semver')
-
---------------------------------------------------------------------------------
-
--- HELPER FUNCTIONS
-
--- | This function converts a @megaparsec@ parser to an @aeson@ parser.
---   Mainly, it handles converting the error output from @megaparsec@ to a
---   string that is appropriate for 'fail'.
-megaparsecToAeson :: Mega.Parsec Mega.Dec Text t
-                  -> (Text -> Aeson.Parser t)
-megaparsecToAeson parser text = case Mega.runParser parser "" text of
-                                  Left  e -> fail (Mega.parseErrorPretty e)
-                                  Right x -> pure x
 
 --------------------------------------------------------------------------------
---
+++ library/Language/Ninja/Eval/Meta.hs
@@ -0,0 +1,125 @@
+-- -*- coding: utf-8; mode: haskell; -*-
+
+-- File: library/Language/Ninja/Eval/Meta.hs
+--
+-- License:
+--     Copyright 2017 Awake Networks
+--
+--     Licensed under the Apache License, Version 2.0 (the "License");
+--     you may not use this file except in compliance with the License.
+--     You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--     Unless required by applicable law or agreed to in writing, software
+--     distributed under the License is distributed on an "AS IS" BASIS,
+--     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--     See the License for the specific language governing permissions and
+--     limitations under the License.
+
+{-# OPTIONS_GHC #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+-- |
+--   Module      : Language.Ninja.Eval.Meta
+--   Copyright   : Copyright 2017 Awake Networks
+--   License     : Apache-2.0
+--   Maintainer  : opensource@awakenetworks.com
+--   Stability   : experimental
+--
+--   A datatype for Ninja top-level variables and other metadata.
+module Language.Ninja.Eval.Meta
+  ( -- * @Meta@
+    Meta, makeMeta, metaReqVersion, metaBuildDir
+  ) where
+
+import           Data.Aeson               as Aeson
+import qualified Data.Aeson.Types         as Aeson
+
+import           Data.Text                (Text)
+
+import           Data.Hashable            (Hashable (..))
+import           GHC.Generics             (Generic)
+
+import qualified Data.Versions            as V
+
+import qualified Text.Megaparsec          as Mega
+
+import           Language.Ninja.Misc.Path
+
+import           Control.Lens.Lens
+
+import           Flow
+
+--------------------------------------------------------------------------------
+
+-- | Ninja top-level metadata, as documented
+--   <https://ninja-build.org/manual.html#ref_toplevel here>.
+data Meta
+  = MkMeta
+    { _metaReqVersion :: !(Maybe V.SemVer)
+    , _metaBuildDir   :: !(Maybe Path)
+    }
+  deriving (Eq, Ord, Show, Generic)
+
+-- | Construct a default 'Meta' value.
+makeMeta :: Meta
+makeMeta = MkMeta
+           { _metaReqVersion = Nothing
+           , _metaBuildDir   = Nothing
+           }
+
+-- | Corresponds to the @ninja_required_version@ top-level variable.
+metaReqVersion :: Lens' Meta (Maybe V.SemVer)
+metaReqVersion = lens _metaReqVersion
+                 $ \(MkMeta {..}) x -> MkMeta { _metaReqVersion = x, .. }
+
+-- | Corresponds to the @builddir@ top-level variable.
+metaBuildDir :: Lens' Meta (Maybe Path)
+metaBuildDir = lens _metaBuildDir
+               $ \(MkMeta {..}) x -> MkMeta { _metaBuildDir = x, .. }
+
+-- | Default 'Hashable' instance via 'Generic'.
+instance Hashable Meta
+
+-- | Converts to @{required-version: …, build-directory: …}@.
+instance ToJSON Meta where
+  toJSON (MkMeta {..})
+    = [ "req-version" .= fmap semverJ _metaReqVersion
+      , "build-dir"   .= _metaBuildDir
+      ] |> object
+    where
+      semverJ :: V.SemVer -> Value
+      semverJ = V.prettySemVer .> toJSON
+
+-- | Inverse of the 'ToJSON' instance.
+instance FromJSON Meta where
+  parseJSON = (withObject "Meta" $ \o -> do
+                  _metaReqVersion <- (o .: "req-version") >>= maybeSemverP
+                  _metaBuildDir   <- (o .: "build-dir")   >>= pure
+                  pure (MkMeta {..}))
+    where
+      maybeSemverP :: Maybe Value -> Aeson.Parser (Maybe V.SemVer)
+      maybeSemverP = fmap semverP .> sequenceA
+
+      semverP :: Value -> Aeson.Parser V.SemVer
+      semverP = withText "SemVer" (megaparsecToAeson V.semver')
+
+--------------------------------------------------------------------------------
+
+-- HELPER FUNCTIONS
+
+-- | This function converts a @megaparsec@ parser to an @aeson@ parser.
+--   Mainly, it handles converting the error output from @megaparsec@ to a
+--   string that is appropriate for 'fail'.
+megaparsecToAeson :: Mega.Parsec Mega.Dec Text t
+                  -> (Text -> Aeson.Parser t)
+megaparsecToAeson parser text = case Mega.runParser parser "" text of
+                                  Left  e -> fail (Mega.parseErrorPretty e)
+                                  Right x -> pure x
+
+--------------------------------------------------------------------------------
