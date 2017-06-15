e436a5480f8247397c64bcd5df5ecfac1d1078d4
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-14 18:07:38 -0700

    Started work on Language.Ninja.Eval.evaluate

--- library/Language/Ninja/Eval.hs
+++ library/Language/Ninja/Eval.hs
@@ -20,8 +20,10 @@
 {-# OPTIONS_GHC #-}
 {-# OPTIONS_HADDOCK #-}
 
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- |
 --   Module      : Language.Ninja.Eval
@@ -35,25 +37,37 @@ module Language.Ninja.Eval
   ( module Language.Ninja.Eval -- FIXME: specific export list
   ) where
 
-import qualified Control.Arrow         as Arr
+import           Control.Arrow
 
 import           Control.Lens.Getter
+import           Control.Lens.Lens
+import           Control.Lens.Setter
 
-import           Language.Ninja.Types
+import           Control.Exception
+import           Control.Monad.Catch
+import           Control.Monad.Except
 
 import           Data.ByteString       (ByteString)
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as BSC8
 
+import           Data.Text             (Text)
 import qualified Data.Text             as T
 import qualified Data.Text.Encoding    as T
 
+import           Data.HashMap.Strict   (HashMap)
 import qualified Data.HashMap.Strict   as HM
+
+import           Data.HashSet          (HashSet)
+import qualified Data.HashSet          as HS
 
 import           Data.Char
 import           Data.Monoid
 
 import           Flow
+
+import           Data.Hashable         (Hashable)
+import           GHC.Generics          (Generic)
 
 import qualified Language.Ninja.AST    as NA
 import qualified Language.Ninja.Parse  as NP
@@ -64,7 +78,91 @@ import qualified Language.Ninja.Types  as NP
 debugNinja :: IO NP.PNinja
 debugNinja = NP.parse "data/build.ninja"
 
-evaluate :: NP.PNinja -> NA.Ninja
-evaluate = undefined
+--------------------------------------------------------------------------------
+
+-- | FIXME: doc
+data EvaluationError
+  = MkEvaluationError
+    { _evalErrorMessage :: !Text
+    }
+  deriving (Eq, Show, Generic)
+
+instance Exception EvaluationError
+
+throwEvaluationError :: (MonadThrow m) => Text -> m a
+throwEvaluationError msg = throwM (MkEvaluationError msg)
+
+--------------------------------------------------------------------------------
+
+evaluate :: forall m. (MonadThrow m) => NP.PNinja -> m NA.Ninja
+evaluate pninja = result
+  where
+    result :: m NA.Ninja
+    result = do
+      meta     <- metaM
+      builds   <- buildsM
+      phonys   <- phonysM
+      defaults <- defaultsM
+      pools    <- poolsM
+      pure (NA.makeNinja
+            & NA.ninjaMeta     .~ meta
+            & NA.ninjaBuilds   .~ builds
+            & NA.ninjaPhonys   .~ phonys
+            & NA.ninjaDefaults .~ defaults
+            & NA.ninjaPools    .~ pools)
+
+    metaM :: m NA.Meta
+    metaM = do
+      -- FIXME: implement this
+      pure NA.makeMeta
+
+    buildsM :: m (HashSet NA.Build)
+    buildsM = (pmultiples <> onHM (first HS.singleton) psingles)
+              |> HM.toList |> mapM evaluateBuild |> fmap HS.fromList
+
+    phonysM :: m (HashMap NA.Target (HashSet NA.Target))
+    phonysM = HM.toList pphonys |> mapM evaluatePhony |> fmap HM.fromList
+
+    defaultsM :: m (HashSet NA.Target)
+    defaultsM = HS.toList pdefaults |> mapM evaluateDefault |> fmap HS.fromList
+
+    poolsM :: m (HashSet NA.Pool)
+    poolsM = HM.toList ppools |> mapM evaluatePool |> fmap HS.fromList
+
+    evaluateTarget :: NP.Str -> m NA.Target
+    evaluateTarget = undefined -- FIXME: implement this
+
+    evaluateBuild :: (HashSet NP.FileStr, NP.PBuild) -> m NA.Build
+    evaluateBuild = undefined -- FIXME: implement this
+
+    evaluatePhony :: (NP.Str, HashSet NP.FileStr)
+                  -> m (NA.Target, HashSet NA.Target)
+    evaluatePhony (name, deps) = do
+      ename <- evaluateTarget name
+      edeps <- HS.fromList <$> mapM evaluateTarget (HS.toList deps)
+      pure (ename, edeps)
+
+    evaluateDefault :: NP.FileStr -> m NA.Target
+    evaluateDefault = undefined -- FIXME: implement this
+
+    evaluatePool :: (NP.Str, Int) -> m NA.Pool
+    evaluatePool = undefined -- FIXME: implement this
+
+    prules     :: HashMap NP.Str NP.PRule
+    psingles   :: HashMap NP.FileStr NP.PBuild
+    pmultiples :: HashMap (HashSet NP.FileStr) NP.PBuild
+    pphonys    :: HashMap NP.Str (HashSet NP.FileStr)
+    pdefaults  :: HashSet NP.FileStr
+    ppools     :: HashMap NP.Str Int
+    prules     = pninja ^. NP.pninjaRules
+    psingles   = pninja ^. NP.pninjaSingles
+    pmultiples = pninja ^. NP.pninjaMultiples
+    pphonys    = pninja ^. NP.pninjaPhonys
+    pdefaults  = pninja ^. NP.pninjaDefaults
+    ppools     = pninja ^. NP.pninjaPools
+
+    onHM :: (Eq k', Hashable k')
+         => ((k, v) -> (k', v')) -> HashMap k v -> HashMap k' v'
+    onHM f = HM.toList .> map f .> HM.fromList
 
 --------------------------------------------------------------------------------
