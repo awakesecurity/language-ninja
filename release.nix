7129446023f99d45c11fa7f7a7e8fb802b9f6ab8
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-16 15:07:51 -0700

    Added hydra-build-products to release.nix

--- release.nix
+++ release.nix
@@ -3,14 +3,32 @@
 with rec {
   pkgs = import nixpkgs nixpkgsArgs;
 
-  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+  inherit (pkgs) haskell;
+
+  addHydraHaddock = hp: pname: (
+    with rec {
+      pkg = hp.${pname};
+      suffix = "share/doc/${pkg.system}-${hp.ghc.name}/${pkg.name}/html";
+    };
+
+    haskell.lib.overrideCabal pkg (old: rec {
+      postInstall = ((old.postInstall or "") + ''
+        mkdir -p "$out/nix-support"
+        echo "doc haddock $out/${suffix} index.html" \
+            > "$out/nix-support/hydra-build-products"
+      '');
+    }));
+
+
+  hp = haskell.packages.${compiler}.override {
     overrides = self: super: {
-      makefile = self.callPackage ./nix/makefile.nix {};
-      versions = self.callPackage ./nix/versions.nix {};
+      makefile       = self.callPackage ./nix/makefile.nix {};
+      versions       = self.callPackage ./nix/versions.nix {};
+      language-ninja = self.callPackage ./nix/language-ninja.nix {};
     };
   };
 };
 
 {
-  language-ninja = haskellPackages.callPackage ./nix/language-ninja.nix {};
+  language-ninja = addHydraHaddock hp "language-ninja";
 }
