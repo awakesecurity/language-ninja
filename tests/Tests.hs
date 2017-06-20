305fac0c3ccb9d5bad334624c9bc6c52200093c6
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-06-20 14:13:40 -0700

    Added test for Language.Ninja.Eval.evaluate

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -46,6 +46,7 @@ import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Except
 
 import qualified Language.Ninja             as Ninja
+import qualified Language.Ninja.Eval        as Ninja
 
 import qualified Test.Hspec                 as H
 import qualified Test.HUnit                 as H
@@ -62,26 +63,42 @@ import qualified Turtle
 dataPrefix :: String
 dataPrefix = "./tests/data/"
 
-roundtrip :: String -> H.Expectation
-roundtrip file = do
+testFiles :: [String]
+testFiles = [ "buildseparate"
+            , "compdb"
+            , "lexical"
+            , "lint"
+            , "nocreate"
+            , "outputtouch"
+            , "phonyorder"
+            , "redefine"
+            , "test1"
+            , "test2"
+            , "test3"
+            , "test4"
+            , "test5"
+            , "test6"
+            ]
+
+parseTestNinja :: String -> IO Ninja.PNinja
+parseTestNinja name = do
   old <- Turtle.pwd
-
   Turtle.cd (FP.decodeString dataPrefix)
+  result <- Ninja.parse (name <> ".ninja")
+  Turtle.cd old
+  pure result
 
-  let inputPath = file <> ".ninja"
-
+roundtripTest :: Ninja.PNinja -> H.Expectation
+roundtripTest pninja = do
   let withTempDir = Turtle.with (Turtle.mktempdir "." "test")
 
   (expected, actual) <- withTempDir $ \tmpdir -> do
-    input  <- Ninja.parse inputPath
-    let prettyInput = Ninja.prettyNinja input
+    let prettyInput = Ninja.prettyNinja pninja
     let tmpfile = tmpdir </> "generated.ninja"
     Turtle.writeTextFile tmpfile prettyInput
     output <- Ninja.parse (FP.encodeString tmpfile)
     let prettyOutput = Ninja.prettyNinja output
     pure (prettyInput, prettyOutput)
-
-  Turtle.cd old
 
   unless (actual == expected) $ do
     -- let actualJ   = Aeson.toJSON actual
@@ -92,28 +109,21 @@ roundtrip file = do
     -- Aeson.encode actualJ `H.shouldBe` Aeson.encode expectedJ
     actual `H.shouldBe` expected
 
-roundtripNames :: [String]
-roundtripNames = [ "buildseparate"
-                 , "compdb"
-                 , "lexical"
-                 , "lint"
-                 , "nocreate"
-                 , "outputtouch"
-                 , "phonyorder"
-                 , "redefine"
-                 , "test1"
-                 , "test2"
-                 , "test3"
-                 , "test4"
-                 , "test5"
-                 , "test6"
-                 ]
+evaluateTest :: Ninja.PNinja -> H.Expectation
+evaluateTest pninja = do
+  Ninja.evaluate pninja
+  pure ()
+
+pninjaTests :: String -> Ninja.PNinja -> H.Spec
+pninjaTests name pninja = do
+  H.describe ("Testing " <> name <> ".ninja") $ do
+    H.it "roundtrip through parser and pretty-printer" (roundtripTest pninja)
+    H.it "evaluate to Ninja" (evaluateTest pninja)
 
 test :: IO ()
 test = H.hspec $ do
-  H.describe "Language.Ninja.Parse.parse" $ do
-    let roundtripTest name = H.it ("roundtrip on " <> name) (roundtrip name)
-    forM_ roundtripNames roundtripTest
+  ninjas <- H.runIO (forM testFiles parseTestNinja)
+  forM_ (zip testFiles ninjas) (uncurry pninjaTests)
 
 main :: IO ()
 main = do
